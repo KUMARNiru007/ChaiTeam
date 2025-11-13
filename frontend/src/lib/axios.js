f058c030278e28a0809db7344cdabbf6b353f24b
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-11-13 22:51:50 +0530

    sset axios issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'production' 
-        ? '/api/v1' 
+    baseURL: 
+      import.meta.env.MODE === 'development' 
+        ? 'http://localhost:3000/api/v1' 
         : 'https://api.chaiteam.in/api/v1',
     withCredentials: true,
 })
